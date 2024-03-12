29e063960d55d3319c6aacdcd08d7c055ba2125f
KristianB09 <kristianb09.kodehode@gmail.com>
2024-03-12 14:05:51 +0100

    functions practice

--- test-php/index.php
+++ test-php/index.php
@@ -1,35 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./styling/php-test.css">
-    <title>Document</title>
-  </head>
-  <body>
-    <h1>Recommended Books</h1>
 
-    <?php $books = [
-      [
-       'name' => 'Do Androids Dream of Electric Sheep',
-        'author' =>'Philip K. Dick',
-        'purchaseUrl' => 'http://example.com'
-      ],
-      [
-        'name'=> 'Project Hail Mary',
-        'author'=> 'Andy Weir',
-        'purchaseUrl'=> 'http://example.com'
-      ]
-    ]; ?>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./styling/php-test.css">
+  <title>Document</title>
+</head>
 
-    <ul>
-      <?php foreach ($books as $book):  ?> 
-        <li>
-          <a href="<?= $book['purchaseUrl'] ?>">
-            <?= $book['name'] ?>
-          </a>
-        </li>
-      <?php endforeach; ?>
-    </ul>
-  </body>
+<body>
+  <h1>Recommended Books</h1>
+
+  <?php $books = [
+    [
+      'name' => 'Do Androids Dream of Electric Sheep',
+      'author' => 'Philip K. Dick',
+      'releaseYear' => 1968,
+      'purchaseUrl' => 'http://example.com'
+    ],
+    [
+      'name' => 'Project Hail Mary',
+      'author' => 'Andy Weir',
+      'releaseYear' => 2021,
+      'purchaseUrl' => 'http://example.com'
+    ],
+    [
+      'name' => 'The Martian',
+      'author' => 'Andy Weir',
+      'releaseYear' => 2011,
+      'purchaseUrl' => 'http://example.com'
+    ]
+  ];
+
+  function filter($items, $fn)
+  {
+
+    $filteredItems = [];
+
+    foreach ($items as $item) {
+
+      if ($fn($item)) {
+
+        $filteredItems[] = $item;
+      }
+    }
+
+    return $filteredItems;
+  }
+
+  $filteredBooks = filter($books, function ($book) {
+
+    return $book['releaseYear'] >= 2000;
+  });
+  ?>
+
+  <ul>
+    <?php foreach ($filteredBooks as $book) :  ?>
+
+      <li>
+        <a href="<?= $book['purchaseUrl'] ?>">
+          <?= $book['name'] ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
+        </a>
+      </li>
+
+    <?php endforeach; ?>
+  </ul>
+</body>
+
 </html>
