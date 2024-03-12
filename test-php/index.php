88abc748b3f71a9eb4ef85613f8d6eebbb83cf54
KristianB09 <kristianb09.kodehode@gmail.com>
2024-03-12 11:16:40 +0100

    simple logic

--- test-php/index.php
+++ test-php/index.php
@@ -3,14 +3,24 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./styling/php-test.css">
     <title>Document</title>
   </head>
   <body>
+    <?php
+        $name = "Dune";
+        $read = true;
+
+        if($read) {
+          $message = "You have read $name";
+        } else {
+          $message = "You have not read $name";
+        }
+
+    ?>
+
     <h1>
-      <?php
-        $greeting = "Hello";
-        echo "$greeting Everybody!";
-      ?>
+      <?= $message; ?>
     </h1>
   </body>
 </html>
