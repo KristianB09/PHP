44887cfcfdaa9d329740d6d736d281979a121af0
kristianb09 <kristianb09.kodehode@gmail.com>
2024-03-17 22:23:31 +0100

    Continuing module 3

---
+++ test-php/Module 3/controllers/notes.php
@@ -0,0 +1,13 @@
+<?php
+
+// connect to mySQL database
+
+$config = require('config.php');
+
+$db = new Database($config['database']);
+
+$heading = "My Notes";
+
+$notes = $db->query('select * from notes where user_id = 1')->fetchAll();
+
+require "./views/notes.view.php";
